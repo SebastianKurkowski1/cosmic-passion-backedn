b0fabb2b9bfa31f17bff22cb906e49a7e830b667
SebastianKurkowski1 <sebastiankurkowski1013@gmail.com>
2023-10-15 22:55:12 +0200

    added new endpoints

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,9 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route("/api/apod/{date}", name: "api_apod")]
-    public function api(RequestHandler $requestHandler, string|null $date = null): JsonResponse
+    public function apiApodDate(RequestHandler $requestHandler, string|null $date = null): JsonResponse
     {
         $data = $requestHandler->nasaApiRequest("apod/$date");
+        return $this->json($data->toArray(), $data->getStatusCode());
+    }
+
+    #[Route("/api/rover/{rover}/{sol}")]
+    public function apiRoverSol(RequestHandler $requestHandler, string $rover, int $sol): JsonResponse
+    {
+        if (!in_array($rover, ['curiosity', 'opportunity', 'spirit', 'perseverance'])) return $this->json('Wrong rover name');
+
+        $data = $requestHandler->nasaApiRequest("mrp-sol/$rover/$sol");
         return $this->json($data->toArray(), $data->getStatusCode());
     }
 }
